b54f8a9ddbbc35aca29b1a8792bbda4beca2ca6a
Nico <45795785+nosoynicoo@users.noreply.github.com>
2019-10-04 01:06:00 +0200

    Update mem.c

--- mem.c
+++ mem.c
@@ -88,11 +88,71 @@ struct fb* mem_first_fit(struct fb* head, size_t size) {
 //-------------------------------------------------------------
 struct fb* mem_best_fit(struct fb* head, size_t size) {
    /* A COMPLETER */ 
+   if(head==null) return NULL;
+    struct fb* p=head;
+    void* adr_avant = p;
+    void* adr_aloue = p->next;
+	
+    do{
+	if (adr_aloue->size >= p->next->size && p->next->size >= size ) {
+	//il y a la place de stoquer notre donnée et si la place et plus petit que ce qu'on a trouvé avant
+		adr_avant = p; //On prend l'adr d'avant pour continuer la chaine
+		adr_aloue = p->next; //On prend la nouveau zone libre jusque la prochaine 
+		p=p->next;//On avance
+	}
+	else{
+		p=p->next; // on passe a la zone libre suivante car pas de place
+	}
+    }while(p->next!=NULL);
+    if(adr_aloue->size - size >= sizeof(struct fb*) ){ 
+	//si il y a la place pour créer une nouvelle zone libre à la suite de la zone à alouer
+			
+		struct fb* zone_libre=adr_aloue+size;//on créer la nouvelle zone libre à la suite de ce qui va être donnée à l'utilisateur
+		zone_libre->size =adr_aloue->size - size ;
+		zone_libre->next = adr_aloue->next;
+		adr_avant->next=zone_libre;
+		return adr_aloue;
+    }
+    else {
+    //si il n'y a pas de place on prend toute sa zone libre
+		adr_avant->next=adr_aloue->next;
+		return adr_aloue;
+    }
     return NULL;
 
 }
 //-------------------------------------------------------------
 struct fb* mem_worst_fit(struct fb* head, size_t size) {
    /* A COMPLETER */ 
+    if(head==null) return NULL;
+    struct fb* p=head;
+    void* adr_avant = p;
+    void* adr_aloue = p->next;
+	
+    do{
+	if (adr_aloue->size <= p->next->size && p->next->size >= size ) {
+	//il y a la place de stoquer notre donnée et si la place et plus grand que ce qu'on a trouvé avant
+		adr_avant = p; //On prend l'adr d'avant pour continuer la chaine
+		adr_aloue = p->next; //On prend la nouveau zone libre jusque la prochaine 
+		p=p->next;//On avance
+	}
+	else{
+		p=p->next; // on passe a la zone libre suivante car pas de place
+	}
+    }while(p->next!=NULL);
+    if(adr_aloue->size - size >= sizeof(struct fb*) ){ 
+	//si il y a la place pour créer une nouvelle zone libre à la suite de la zone à alouer
+			
+		struct fb* zone_libre=adr_aloue+size;//on créer la nouvelle zone libre à la suite de ce qui va être donnée à l'utilisateur
+		zone_libre->size =adr_aloue->size - size ;
+		zone_libre->next = adr_aloue->next;
+		adr_avant->next=zone_libre;
+		return adr_aloue;
+    }
+    else {
+    //si il n'y a pas de place on prend toute sa zone libre
+		adr_avant->next=adr_aloue->next;
+		return adr_aloue;
+    }
     return NULL;
 }
